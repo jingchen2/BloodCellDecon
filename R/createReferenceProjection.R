aaf758aaddfda81f314880b7aee76e39faec3758
jingchen2 <jing.chen2@cchmc.org>
2023-11-09 17:43:15 -0500

    Initial commit

---
+++ R/createReferenceProjection.R
@@ -0,0 +1,59 @@
+#' Create projection matrix and reference profiles from a beta matrix of reference samples.
+#'
+#' @param beta.ref A matrix of beta values. Probes are in rows, samples in columns. Row names have to be probe IDs.
+#' @param cell.type A vector of cell names. Should have the same length as columns of beta.ref.
+#' @param n.top.probe Number of probes for each cell type to be included for the projection.
+#' @param top.probes A vector of probe IDs. If provided, no probe selection will be performed and the projection will be created based on these probes.
+#'
+#' @return A list with 3 elements:
+#' * "projection" is the projection matrix to convert selected probes to PCA space.Row names have to be probe IDs. Columns are PCs;
+#' * "ref.beta.mat" is the matrix of mean beta values for the reference cells. Rows are probes, columns samples. Row names have to be probe IDs;
+#' * "pca_res" is the PCA result from prcomp.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'ref.projection.EPIC = createReferenceProjection(beta.ref=ref.beta, cell.type = ref.cell,n.top.probe = 150)
+#' }
+createReferenceProjection=function(beta.ref, cell.type, n.top.probe=150, top.probes=NULL){
+  stopifnot(is.matrix(beta.ref),ncol(beta.ref)==length(cell.type))
+
+  # perform probe selection
+  if(is.null(top.probes)){
+    cells = unique(cell.type)
+    # perform row t-test
+    probe.stat.list = lapply(cells,function(x){
+      print(paste0('Probe selection for ',x))
+      tstat=genefilter::rowttests(beta.ref,as.factor(cell.type==x), tstatOnly = TRUE)
+      y=tstat$statistic
+      names(y)=rownames(tstat)
+      y
+    })
+
+    # select top probes for PCA based on t-stat
+    top.probe.list=lapply(probe.stat.list,function(x){
+      order(x)[c(1:n.top.probe/2, (length(x)-n.top.probe/2+1):length(x))]
+    })
+
+    # create ref eset
+    all.top.probes = unique(unlist(top.probe.list))
+  }
+
+  else
+    all.top.probes = top.probes
+  beta.ref = beta.ref[all.top.probes,]
+
+  # PCA of ref eset
+  pca_res = stats::prcomp(t(beta.ref), scale. = F, center = F)
+
+  projection.id=rownames(pca_res$rotation)
+  projection = pca_res$rotation
+  # create ref.betas as rowMeans
+  ref.betas = tapply(1:ncol(beta.ref),cell.type,function(i){
+    rowMeans(beta.ref[,i])
+  })
+  ref.beta.mat=as.data.frame(do.call(cbind,ref.betas))
+  colnames(ref.beta.mat)=names(ref.betas)
+
+  list(projection=projection, ref.beta.mat=ref.beta.mat,pca.res=pca_res)
+}
---
+++ R/estimateCellComposition.R
@@ -0,0 +1,52 @@
+#' Estimate cell compostions in test samples based on reference profile and projection.
+#'
+#' @param test.beta Beta matrix of samples to be tested. Probes are in rows. Samples are in columns. Row names have to be probe IDs.
+#' @param ref.beta.mat Matrix of mean beta values for the reference cells. Row names have to be probe IDs. For EPIC arrays,
+#' ref.projection.EPIC$ref.beta.mat, pre-calculated from GSE167998, can be used.
+#' @param projection Projection matrix to convert selected probes to PCA space. Row names have to be probe IDs. Columns are PCs.For EPIC arrays,
+#' ref.projection.EPIC$projection, pre-calculated from GSE167998, can be used.
+#' @param n.PC number of PCs to include for deconvolution.
+#' @param extended Boolean variable to indicate whether to include Bcell, CD4T, CD8T and Gran in output.
+#'
+#' @return A matrix of cell compositions in the test samples. Samples are in rows, cell types in columns.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' test.res=estimateCellComposition(test.beta = test.beta, ref.beta.mat = ref.projection.EPIC$ref.beta.mat,projection = ref.projection.EPIC$projection, n.PC = 20,extended = F)
+#' plot_celltype(test.res*100,test.pd*100,celltype = 'Bmem')
+#' }
+estimateCellComposition=function(test.beta, ref.beta.mat, projection, n.PC=20,extended=TRUE){
+  # find overlap between test and ref
+  common.pid = intersect(rownames(ref.beta.mat),rownames(test.beta))
+  print(paste0(length(common.pid),' probes found in test data.'))
+  # create overlapping matrices and vectors
+  projection0 = projection[match(common.pid,rownames(projection)),1:n.PC]
+  ref.beta.mat = data.matrix(ref.beta.mat[match(common.pid,rownames(ref.beta.mat)),])
+  test.beta = t(test.beta[match(common.pid,rownames(test.beta)),])
+  projection = projection[match(common.pid,rownames(projection)),]
+
+  # estimate cell composition for each sample
+  test.res=t(apply(test.beta,1,function(test0){
+    y=crossprod(projection[,1:n.PC], test0)
+    X=crossprod(projection[,1:n.PC], ref.beta.mat)
+
+    cp_X_pd = Matrix::nearPD(crossprod(X), base.matrix = TRUE)
+
+    if (!isTRUE(cp_X_pd$converged))
+      stop("Could not find nearest positive definite matrix.")
+    res = quadprog::solve.QP(Dmat = cp_X_pd$mat, dvec = crossprod(y,X),
+                             Amat = cbind(1, diag(ncol(X))), bvec = c(1, rep(0,ncol(X))), meq = 1)
+    stats::setNames(round(res$solution, 7), colnames(ref.beta.mat))
+  }))
+
+  test.res=as.data.frame(test.res)
+  if(extended){
+    test.res$Bcell=test.res$Bmem+test.res$Bnv
+    test.res$CD4T = test.res$CD4mem + test.res$CD4nv + test.res$Treg
+    test.res$CD8T = test.res$CD8mem + test.res$CD8nv
+    test.res$Gran = test.res$Bas + test.res$Eos + test.res$Neu + test.res$Mono
+  }
+  test.res
+}
